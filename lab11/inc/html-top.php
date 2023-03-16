ef27ae1befeb43256a48050cde1d360e156f0eb1
Zian Kang <114457597+YetStillAGod@users.noreply.github.com>
2023-03-16 13:09:08 -0400

    homework 11

---
+++ lab11/inc/html-top.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lab 11 - Newton</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/navigation.css">
+</head>
---
+++ lab11/inc/nav.php
@@ -0,0 +1,8 @@
+<nav class="menu">
+		<ul>
+			<li><a href="newton.php">About Newton</a></li>
+			<li><a href="works.php">His Works</a></li>
+			<li><a href="life.php">His Later Life</a></li>
+			<li><a href="publishes.php">His Publishes</a></li>
+		</ul>
+	</nav>
