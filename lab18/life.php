b3972df925371dca21c463eea8e0188dc0e67125
Zian Kang <114457597+YetStillAGod@users.noreply.github.com>
2023-04-10 02:12:19 -0400

    lab 18

---
+++ lab18/life.php
@@ -0,0 +1,35 @@
+
+<?php
+	$customCSS = "<link rel='stylesheet' type='text/css' href='css/styles.css'>
+	<link rel='stylesheet' type='text/css' href='css/navigation.css'>";
+	include "inc/html-top.php"
+?>
+
+<body>
+<div class="container">
+	<header>
+		<h1>Isaac Newton</h1>
+
+		<div>The Physicist</div>
+	</header>
+	<? include "inc/nav.php" ?>
+	<article>
+		<h2>His Later Life</h2>
+
+		<figure>
+			<img src = "images/telescope.jpg" alt = "telescope">
+			<figcaption>Picture of the Telescope</figcaption>
+		</figure>
+
+		<p>Newton moved to London to take up the post of warden of the Royal Mint in 1696, a position that he had obtained through the patronage of Charles Montagu, 1st Earl of Halifax, then Chancellor of the Exchequer. He took charge of England's great recoining, trod on the toes of Lord Lucas, Governor of the Tower, and secured the job of deputy comptroller of the temporary Chester branch for Edmond Halley. Newton became perhaps the best-known Master of the Mint upon the death of Thomas Neale in 1699, a position Newton held for the last 30 years of his life. These appointments were intended as sinecures, but Newton took them seriously. He retired from his Cambridge duties in 1701, and exercised his authority to reform the currency and punish clippers and counterfeiters. </p>
+
+		<p>As a result of a report written by Newton on 21 September 1717 to the Lords Commissioners of His Majesty's Treasury, the bimetallic relationship between gold coins and silver coins was changed by royal proclamation on 22 December 1717, forbidding the exchange of gold guineas for more than 21 silver shillings. This inadvertently resulted in a silver shortage as silver coins were used to pay for imports, while exports were paid for in gold, effectively moving Britain from the silver standard to its first gold standard. It is a matter of debate as to whether he intended to do this or not. It has been argued that Newton conceived of his work at the Mint as a continuation of his alchemical work. </p>
+	</article>
+	<aside>
+		<h2>There is more</h2>
+		<p>Newton died in his sleep in London on 20 March 1727 (OS 20 March 1726; NS 31 March 1727). He was given a ceremonial funeral, attended by nobles, scientists, and philosophers, and was buried in Westminster Abbey among kings and queens. He is also the first scientist to be buried in the abbey. Voltaire may have been present at his funeral. A bachelor, he had divested much of his estate to relatives during his last years, and died intestate. His papers went to John Conduitt and Catherine Barton. After his death, Newton's hair was examined and found to contain mercury, probably resulting from his alchemical pursuits. Mercury poisoning could explain Newton's eccentricity in late life</p>
+	</aside>
+</div>
+<?php include "inc/scripts.php" ?>
+</body>
+</html>
